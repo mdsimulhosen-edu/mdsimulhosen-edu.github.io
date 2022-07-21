43c63269b60c4612d8797483cbad001dd81a5139
Md Simul Hosen <55944650+mdsimulhosen@users.noreply.github.com>
2022-07-22 01:36:56 +0600

    My Project with HTML, CSS, JS, BS

---
+++ bs-Chaka/slick/js/feedback.js
@@ -0,0 +1,36 @@
+$('.feedback').slick({
+    dots:true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplayspeed: 100,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots:true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
